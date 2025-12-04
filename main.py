476809d14b0d120b64c0e8d0d7f7d4d55ac333f2
Angelina Zelenskaya <angel.zel@mail.ru>
2025-12-04 16:28:49 +0300

    second commit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
+from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.lang import Builder
 
 
 class Recipe(Widget):
@@ -7,7 +9,16 @@ class Recipe(Widget):
 
 class RecipeApp(App):
     def build(self):
-        return Recipe()
+        sm = ScreenManager()
+        Builder.load_file('styles/backgrounds.kv')
+        main_screen = MainScreen(name='main')
+        recipe_widget = Recipe()
+        main_screen.add_widget(recipe_widget)
+        sm.add_widget(main_screen)
+        return sm
+
+class MainScreen(Screen):
+    pass
 
 
 if __name__ == '__main__':
