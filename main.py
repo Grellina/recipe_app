cf811cdc3921bcf6e2f54a29fbc5b5560785ce3b
Angelina Zelenskaya <angel.zel@mail.ru>
2025-12-08 14:55:34 +0300

    added a recipe page

--- main.py
+++ main.py
@@ -1,21 +1,21 @@
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.uix.screenmanager import Screen, ScreenManager
+from kivy.uix.screenmanager import Screen, ScreenManager, FadeTransition
 from kivy.lang import Builder
 
+Builder.load_file('styles/main_screen.kv')
+Builder.load_file('styles/recipe_screen.kv')
 
-class Recipe(Widget):
-    pass
+
 
 class RecipeApp(App):
     def build(self):
         sm = ScreenManager()
-        Builder.load_file('styles/main_screen.kv')
-        Builder.load_file('styles/recipe_screen.kv')
         main_screen = MainScreen(name='main')
-        recipe_widget = Recipe()
-        main_screen.add_widget(recipe_widget)
+        recipe_screen = RecipeScreen(name='recipe')
         sm.add_widget(main_screen)
+        sm.add_widget(recipe_screen)
+        sm.current = 'main'
+        sm.transition = FadeTransition(duration= 0.3)
         return sm
 
 class MainScreen(Screen):
